813aa047cbdff894e55ed175423f7e23e282aad1
Abdelrahman <35752239+AbdelrahmanSherifHadeya@users.noreply.github.com>
2020-03-20 15:16:38 +0300

    Create hello.c

---
+++ pset1/hello.c
@@ -0,0 +1,9 @@
+//Small program to hello people by their names
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("what is your name?\n");
+    printf("hello, %s\n", name);
+}
